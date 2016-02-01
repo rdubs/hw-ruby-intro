8cf7733474efdfdc25bb4d0d39bd378d7632cf87
rdubs <ranitdubey@gmail.com>
2016-02-01 07:17:15 +0000

    hw0 tests pass

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,67 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0) { |result, element| result + element }
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.sort.last(2).reduce(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
-end
-
+  if arr.length == 0 and n == 0
+    return true
+  end
+  # return false if arr.empty? || arr.size == 1
+  l_index = 0
+  r_index = arr.length - 1
+  arr.sort!
+  while l_index < r_index do
+    target = arr[l_index] + arr[r_index]
+    if target == n
+      return true
+    elsif arr[l_index] + arr[r_index] < n
+      l_index = l_index + 1
+    else
+      r_index = r_index - 1
+    end
+  end
+  return false
+end 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /[a-zA-Z]/.match(/\A[^aeiouAEIOU\d]/.match(s).to_s)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if /\A[01]+/.match(s)
+    num = s.to_i(2)
+    return num % 4 == 0
+  else 
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn.empty? or price <= 0
+      raise ArgumentError.new("Price must be >= 0 and isbn must not be empty string")
+      return
+    end
+    @isbn = isbn
+    @price = price
+  end
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def price_as_string
+    "$#{'%.2f' % @price}"
+  end
 end
